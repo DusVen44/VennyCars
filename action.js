104c90546d8a355a390c2c0003db1f727e85fd39
Dustin Venable <Venny44@yahoo.com>
2020-04-17 15:28:49 -0500

    More Tweaks for Mobile

--- action.js
+++ action.js
@@ -1,9 +1,14 @@
-//Function to show Minimal View
+//Function to toggle View Modes
 function minimal() {
     $(".navbar").on("click", ".view-mode", function() {
         $("iframe").toggle();
         $(".car").toggleClass("small-car");
-    })
+        if (this.innerHTML == "Show Minimal View") {
+            this.innerHTML = "Show Full View";
+        } else {
+            this.innerHTML = "Show Minimal View";
+        };
+    });
 }
 
 minimal();
